b35fdb266ed21a49c1192b39759d873816ac6426
Jose Luis Estrada <joseluis.estr@gmail.com>
2026-05-23 21:02:01 +0000

    Created stating file for payment, two sub folders in the mart folder (finance, and marketing), created fct_orders.sql under finance, and moved dim_customers.sql under marketing

---
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -0,0 +1,19 @@
+WITH
+SOURCE AS(
+    SELECT * FROM raw.stripe.payment
+),
+
+RENAMED AS (
+    SELECT
+        id AS payment_id,
+        orderid AS order_id,
+        paymentmethod AS payment_method,
+        status AS payment_status,
+        amount AS payment_amount,
+        created AS payment_created,
+        _batched_at
+
+    FROM SOURCE
+)
+
+SELECT * FROM RENAMED
